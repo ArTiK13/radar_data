9a7806a2b4633816b0fea489303a01c9176982da
Vladimir Filipenko <vovaf709@yandex.ru>
2025-02-12 14:17:50 +0300

    Add filter composer

--- radar_data/filters.py
+++ radar_data/filters.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from typing import Sequence
 
 from .types import Scene
 
@@ -11,6 +12,21 @@ class BaseFilter(ABC):
     @abstractmethod
     def apply(self, scene: Scene) -> Scene:
         pass
+
+
+class Compose(BaseFilter):
+    """
+    Compose several fitlers
+
+    Given (filter_1, filter_2, ..., fitler_n) applies them sequentially
+    """
+    def __init__(self, filters: Sequence[BaseFilter]):
+        self.filters = filters
+
+    def apply(self, scene: Scene) -> Scene:
+        for filter_ in self.filters:
+            scene = filter_.apply(scene)
+        return scene
 
 
 class IdentityFilter(BaseFilter):
