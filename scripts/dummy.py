285a21ec0e37147f0dce080ca1a51b9b43f3b0cb
ArtemK <ubileyka19@gmail.com>
2025-02-12 18:35:09 +0300

    fix .csv parsing to .json

--- scripts/dummy.py
+++ scripts/dummy.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from typing import Optional
 from radar_data.parser import parsing_frames
 import radar_data.filters
-
+import json
 
 if __name__ == "__main__":
     parser = ArgumentParser()
@@ -21,7 +21,7 @@ if __name__ == "__main__":
 
     radar_df, lidar_df = parsing_frames(args.input_scene)
 
-    print(lidar_df, radar_df)
+    # print(lidar_df, radar_df)
 
     # Prepare filter with `args.filter` and `args.filter_args`
     scene_filter = getattr(import_module("radar_data.filters"), args.filter)()
@@ -30,6 +30,38 @@ if __name__ == "__main__":
     radar_df = scene_filter.apply(radar_df)
 
     # Save scene (just copy for this example)
-    radar_df.to_csv(str(args.output_scene).replace(".json", ".csv"))
+    radar_tags = """X_RAW, (m)
+Y_RAW, (m)
+Z, (m)
+AbsoluteRadialVelocity
+RadarCrossSection
+RelativeRadialVelocity
+RelativeLateralVelocity
+Range
+DistanceAccuracy
+AngleAccuracy
+DynProp
+HasQuality
+QPDH0
+QDistLongRMS
+QDistLatRMS
+QVLongRMS
+QVLatRMS
+QAmbigState
+QInvalidState
+(radar_point_ts - lidar_ts), (s)
+radar_idx""".split(
+        "\n"
+    )
+    radar_df = radar_df[radar_tags]
+    # print(type(lidar_df))
+    of = {
+        "lidar": tuple(map(tuple, lidar_df.values)),
+        "radar": tuple(map(tuple, radar_df.values)),
+    }
+    # print(of)
+    with open(str(args.output_scene), "w") as file:
+        json.dump(of, file)
+    # radar_df.to_csv(str(args.output_scene).replace(".json", ".csv"))
 
     # shutil.copy(args.input_scene, args.output_scene)
